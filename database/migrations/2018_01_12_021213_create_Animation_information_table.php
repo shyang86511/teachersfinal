fa20a5e36f022d09a53a4d2c195090e38ef09b34
F308-PC-624\F308USER <shyang8605113@gmail.com>
2018-01-19 08:41:14 +0800

    no message

---
+++ database/migrations/2018_01_12_021213_create_Animation_information_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnimationInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Animation_informations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('animation_classification_id')->unsigned();
+            $table->foreign('animation_classification_id')
+                  ->references('id')
+                  ->on('Animation_classification')
+                  ->onDelete('cascade');
+            $table->string('Original_author');
+            $table->string('Manufacturer');
+            $table->timestamps();
+            $table->softDeletes();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Animation_information');
+    }
+}
